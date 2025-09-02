6d1aa9670879332957843a51dedaf0502f4140c3
AMMAD DIN <ammaddin2@carecloud.com>
2025-09-02 15:33:28 +0500

    Login Page completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,39 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const metropolis = localFont({
+  src: [
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-metropolis",
+  display: "swap",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +57,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${metropolis.variable}`}
+      >
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import Login from "@/Components/LoginPage/LoginContainer";
 
 export default function Home() {
   return (
-   <>
-   </>
+    <>
+      <Login />
+    </>
   );
 }
